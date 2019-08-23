5061d18f83efd48f5a63cf4190969988b466f4ae
James Priest <james-priest@outlook.com>
2019-08-22 21:21:08 -0700

    feat: Save user to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
-import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
   apiKey: 'AIzaSyASDQRXc7hWYncV_x7oCAVbYUVcpFHvHvA',
@@ -12,15 +12,48 @@ const config = {
   appId: '1:272818249661:web:857dbd91088b92e4'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // Get docRef from 'users' collection with the given 'uid'
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // console.log(userRef);
+
+  // Get snapshot of user document from docRef
+  const snapShot = await userRef.get();
+  // console.log(snapShot);
+
+  // if the record doesn't exist then .set() it
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-googleAuthProvider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(googleAuthProvider);
+// Google Authentication
+const glProvider = new firebase.auth.GoogleAuthProvider();
+glProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(glProvider);
 
-// const ghAuthProvider = new firebase.auth.GithubAuthProvider();
+// GitHub Authentication
+// const ghProvider = new firebase.auth.GithubAuthProvider();
 
 export default firebase;
